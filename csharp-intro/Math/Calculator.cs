aa69f3a845d946968143a97fba83b984789fa0e4
Ara <ayaghsizian@gmail.com>
2017-11-22 18:48:56 -0500

    made a Math folder with a Calculator class and used the calculator

---
+++ csharp-intro/Math/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_intro.Math
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- csharp-intro/Program.cs
+++ csharp-intro/Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System; // So I can use Console
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using csharp_intro.Math;
+
 
 // CTRL f5 to run
 // CTRL Shift B to check
@@ -20,7 +23,11 @@ namespace csharp_intro
             zara.Introduce();
 
 
+            Console.WriteLine("hi");
 
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(3, 7);
+            Console.WriteLine(result);
 
             //var a = 10;
             //var b = 3;
@@ -39,7 +46,7 @@ namespace csharp_intro
             //    Console.WriteLine("number could not be converted to a byte");
             //}
 
-           
+
 
 
             // Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
@@ -47,7 +54,7 @@ namespace csharp_intro
             //const float Pi = 3.14f;
             //Console.WriteLine(Pi);
 
-            
+
             // int i = 1000;
             // byte b = (byte)i; // Doing a cast
             // Console.WriteLine(b);
