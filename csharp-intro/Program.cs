bc9136261b6391a0aa418ab85d01746bcac44fe8
Ara <ayaghsizian@gmail.com>
2017-11-30 20:14:10 -0500

    debugging practice, to be continued tomorrow, 10-78

--- csharp-intro/Program.cs
+++ csharp-intro/Program.cs
@@ -21,8 +21,19 @@ namespace csharp_intro
         {
 
             // Debugging practice
+            var numbers = new List<int> {1,2,3,4,5,6};
+            var smallests = GetSmallests(numbers, 3);
 
+            foreach (var number in smallests)
+            {
+                Console.WriteLine(number);                
+            }
         }
 
+
+        // to be continued 10-78
+        public static List<int> GetSmallests(List<int> list, int count)
+
+        public static int GetSmallest(List<int> list)
     }
 }
